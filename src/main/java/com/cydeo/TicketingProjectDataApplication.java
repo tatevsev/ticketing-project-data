1e56aba48b1789e09d499b98fe419160143104ee
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-10-31 21:42:32 +0100

    initial commit

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketingProjectOrmApplication {
+public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketingProjectOrmApplication.class, args);
+        SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
 }
