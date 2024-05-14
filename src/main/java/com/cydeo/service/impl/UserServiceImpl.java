b7d73234cf56514b3ed58e65ff24a05869ffe321
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-05-14 23:37:18 +0400

    Commits

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,20 +1,24 @@
 package com.cydeo.service.impl;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
 
@@ -23,17 +27,20 @@ public class UserServiceImpl implements UserService {
 
        List <User> userList =  userRepository.findAll(Sort.by("firstName"));
 
-
-        return List.of();
+        return  userList.stream().map(userMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
     public UserDTO findByUserName(String userName) {
+       // userMapper.convertToDto(userRepository.findByUserName(userName));
+
         return null;
     }
 
     @Override
     public void save(UserDTO user) {
+
+        userRepository.save(userMapper.convertToEntity(user));
 
     }
 
