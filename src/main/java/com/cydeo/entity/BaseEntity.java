b9f6d0aeeceeea8ef2f705a1442fef6ebe2b733f
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-05-16 20:26:09 +0400

    Commits

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
@@ -20,16 +17,28 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Boolean isDeleted=false;
+   // @Column(nullable = false, updatable = false)
     private LocalDateTime insertDateTime;
+  //  @Column(nullable = false, updatable = false)
     private Long insertUserId;
+   // @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+  //  @Column(nullable = false)
     private Long lastUpdateUserId;
 
+    @PrePersist
     private void onPrePersist(){
         this.insertDateTime = LocalDateTime.now();
         this.lastUpdateDateTime = LocalDateTime.now();
         this.insertUserId = 1L;
         this.lastUpdateUserId=1L;
     }
+
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
+
 
 }
