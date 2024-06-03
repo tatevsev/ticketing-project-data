6fbb28adfd7db529542164a30aa4a5964a9e06a8
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-06-03 23:13:39 +0400

    Security Commits

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,4 +29,30 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(userList);
     }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.
+                authorizeRequests()//this make all our pages authorized
+                .antMatchers(
+                        "/",
+                        "/login",
+                        "/fragments/**",
+                        "/assents/**",
+                        "/images/**"
+                ).permitAll() //anyone can access
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and().build();
+
+
+
+    }
+
+
+
+
 }
